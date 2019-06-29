3c243650012548e7052a80910ab631f9c0581fa4
Akash Chakroborty <akashranjanchakroborty@gmail.com>
2019-06-29 13:22:45 +0530

    added Navbar component

---
+++ mern-exercise-tracker/src/components/exercises-list.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class ExercisesList extends Component {
+
+    render() {
+      return (
+          <h1>exercise-list</h1>
+      );
+    }
+}
